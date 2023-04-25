0076d31ab760e8e6f8d7f11255c16bb79d2fa21d
emilmair <emil.mair@posteo.net>
2023-04-25 16:07:43 +0200

    fix random seed

--- src/rng.c
+++ src/rng.c
@@ -4,9 +4,14 @@
 #include <time.h>
 
 void rng_init(void) {
-    srand(time(NULL));
+    unsigned short tmp[3];
+    tmp[0]=time(NULL);
+    tmp[1]=time(NULL);
+    tmp[2]=time(NULL);
+    seed48(tmp);
 }
 
 uint32_t rng_u32(void) {
     return (uint32_t) lrand48();
 }
+
